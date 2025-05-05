48560585460417a87dea22c2ac7884cd52580756
Gowtham <venthan89@gmail.com>
2025-05-05 21:44:37 +0800

    word counter game with maps learned the for each loop with _ fields can empty strings Field and lower scanner revision

---
+++ word-counter-maps/go.mod
@@ -0,0 +1,3 @@
+module word-counter-map
+
+go 1.24.2
---
+++ word-counter-maps/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	Scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Enter the sentence")
+
+	Scanner.Scan()
+
+	input := Scanner.Text()
+
+	//get user input and scan it
+	lower := strings.ToLower(input)
+
+	//making to to lower case
+
+	words := strings.Fields(lower)
+
+	//Storing as String in a slice
+
+	//daclare a map
+
+	wordcount := make(map[string]int)
+
+	//get word count of the entered sentence by looping all the words buddy
+
+	//word directly declared in the loop
+	//The _ ignores the index (we don’t need it here).
+
+	for _, word := range words {
+		wordcount[word]++
+	}
+
+	//finaly venthan print the sentence enter with the values
+	fmt.Println("\n words frquency count in the sentence you entered Mr.Venthan")
+
+	for word, count := range wordcount {
+
+		fmt.Printf(" %s <---> %d", word, count)
+		fmt.Println()
+	}
+
+}
